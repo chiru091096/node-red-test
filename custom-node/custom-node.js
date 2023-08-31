3ab8c989ca3bd0caa7772d71e19d7a34936b0206
chiru091096 <chiru091096@gmail.com>
2023-08-31 20:33:48 +0530

    Update custom-node.js

--- custom-node/custom-node.js
+++ custom-node/custom-node.js
@@ -3,7 +3,6 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
         node.on('input', function(msg) {
-            msg.payload = {"key" : "dummy"};
             node.send(msg);
         });
     }
